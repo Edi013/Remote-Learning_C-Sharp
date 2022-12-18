4a76bc20646cb3752b095cb4275994fa5a173e2e
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2022-12-18 13:11:15 +0200

    Simplified BuyView's method

--- Homework3_Buy_Use_Case+Exceptions/DataLayer/ProductRepository.cs
+++ Homework3_Buy_Use_Case+Exceptions/DataLayer/ProductRepository.cs
@@ -4,36 +4,8 @@ namespace iQuest.VendingMachine.DataLayer
     {
         private static List<Product> products;
 
-       public static int MinColumnNumber(){
-            int min = products[0].ColumnId;
-
-            foreach(Product product in products)
-            {
-                if (product.ColumnId < min)
-                    min = product.ColumnId;
-            }
-
-            return min;   
-        }
-       public static int MaxColumnNumber(){
-            int max = products[0].ColumnId;
-
-            foreach(Product product in products)
-            {
-                if (product.ColumnId > max)
-                    max = product.ColumnId;
-            }
-
-            return max;
-        }
-
         public  ProductRepository()
         {
-            // Daca utilizam o lista din afara:
-            // parametru: List<Product> listOfProducts
-            // ListOfProducts = listOfProducts;
-
-            // Daca utilizam o lista interna:
             products = new List<Product>() {
                 new Product(1, "7Days", 4.99F, 10),
                 new Product(2, "Rolls", 3.99F, 7),
@@ -46,7 +18,6 @@ namespace iQuest.VendingMachine.DataLayer
             return products;
         }
 
-        
         public Product? GetProductByColumnId(int columnId)
         {
             foreach(Product product in products)
--- Homework3_Buy_Use_Case+Exceptions/PresentationLayer/BuyView.cs
+++ Homework3_Buy_Use_Case+Exceptions/PresentationLayer/BuyView.cs
@@ -6,38 +6,28 @@ namespace iQuest.VendingMachine.PresentationLayer
     internal class BuyView : DisplayBase
     {
 
-        public int RequestProduct()
+         public int RequestProduct()
+    {
+        int columnNumber;
+
+        Console.WriteLine();
+        DisplayLine($"Type the column number of the desired product:", ConsoleColor.White);
+        Console.WriteLine();
+        
+        string userInput= Console.ReadLine();
+
+        if(userInput == "") // Test for userInput=" ". 
         {
-            int columnNumber;
-            int minColumnNumber = ProductRepository.MinColumnNumber();
-            int maxColumnNumber = ProductRepository.MaxColumnNumber();
-
-            Console.WriteLine();
-            Console.WriteLine();
-            DisplayLine($"Type the column number of the desired product: ({minColumnNumber} - {maxColumnNumber})",
-                ConsoleColor.White);
-            Console.WriteLine();
-            
-            string userInput= Console.ReadLine();
-
-            if(userInput == "")
-            {
-                throw new CancelException();
-            }
-            if(int.TryParse(userInput, out columnNumber))
-            {
-                if(columnNumber < minColumnNumber || columnNumber > maxColumnNumber)
-                {
-                    throw new InvalidColumnNumberException();
-                }
-            }
-            else
-            {
-                throw new InvalidInputException();
-            }
-            
-            return columnNumber;
+            throw new CancelException();
         }
+
+        if(!int.TryParse(userInput, out columnNumber))
+        {
+            throw new InvalidInputException();
+        }
+
+        return columnNumber;
+    }
 
         public void DispenseProduct(string productName)
         {
