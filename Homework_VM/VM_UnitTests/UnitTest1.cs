8a8494b8aa2bd97606d13367b0ef35c7ad2b220b
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2022-12-19 11:24:13 +0200

    Added a xUnit Test Project and biding it with VM's project.
    VM_UnitTests project created
    
    Splitting VM and VM_UnitTests in unitary folders and binding them under the same sln

---
+++ Homework_VM/VM_UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace VM_UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("project unittest works");
+        }
+    }
+}
