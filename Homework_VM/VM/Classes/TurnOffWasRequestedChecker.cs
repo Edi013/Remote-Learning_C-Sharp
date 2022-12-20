621f29078d0baa8fb9f9c197097a9f3079f37fe8
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2022-12-20 11:43:56 +0200

    Decoupling VendingMachineApplication class and TurnOff system
    Created a new class with its own Interface, it is a checker.

---
+++ Homework_VM/VM/Classes/TurnOffWasRequestedChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iQuest.VendingMachine.Interfaces;
+
+namespace iQuest.VendingMachine.Classes
+{
+    internal class TurnOffWasRequestedChecker : ITurnOffWasRequestedChecker
+    {
+        public bool TurnOffWasRequestedStatus { get; set; }
+
+        public TurnOffWasRequestedChecker()
+        {
+            TurnOffWasRequestedStatus = false;
+        }
+
+        public void TurnOff()
+        {
+            TurnOffWasRequestedStatus = true;
+        }
+
+        
+    }
+}
---
+++ Homework_VM/VM/Interfaces/ITurnOffWasRequestedChecker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iQuest.VendingMachine.Interfaces
+{
+    internal interface ITurnOffWasRequestedChecker
+    {
+        public bool TurnOffWasRequestedStatus { get; set; }
+        public void TurnOff();
+    }
+}
