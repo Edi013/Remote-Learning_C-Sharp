10e5b8be9560e6741e39c95985f5abfc3609b868
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-01-05 22:04:11 +0200

    added the IPaymentUseCase interface and it's class

---
+++ Homework_VM/VM/Interfaces/IPaymentUseCase.cs
@@ -0,0 +1,14 @@
+﻿namespace iQuest.VendingMachine.Interfaces
+{
+    internal interface IPaymentUseCase
+    {
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public bool CanExecute { get; }
+
+        public void Execute(float price);
+    }
+}
+
