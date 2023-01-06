0fc901bc86c4589517f89df68be0f9caf9bcaec0
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-01-06 13:00:07 +0200

    Created user input for choosing payment method

--- Homework_VM/VM/PresentationLayer/IBuyView.cs
+++ Homework_VM/VM/PresentationLayer/IBuyView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iQuest.VendingMachine.UseCases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace iQuest.VendingMachine.PresentationLayer
     {
         public int RequestProduct();
         public void DispenseProduct(string productName);
+        public int AskForPaymentMethod(IEnumerable<PaymentMethod> paymentMethods);
     }
 }
---
+++ Homework_VM/VM/UseCases/PaymentUseCase/PaymentUseCase.cs
@@ -0,0 +1,35 @@
+﻿using iQuest.VendingMachine.Interfaces;
+using iQuest.VendingMachine.PresentationLayer;
+using iQuest.VendingMachine.Services;
+
+namespace iQuest.VendingMachine.UseCases
+{
+    internal class PaymentUseCase : IPaymentUseCase
+    {
+        private readonly IBuyView buyView;
+        private readonly IAuthenticationService authenticationSerivce;
+
+        private readonly List<PaymentMethod> paymentMethods;
+
+        public PaymentUseCase(IBuyView buyView, IAuthenticationService authenticationSerivce) 
+        {
+            this.authenticationSerivce = authenticationSerivce;
+            this.buyView = buyView;
+            paymentMethods =
+                new List<PaymentMethod>()
+                {
+                    new PaymentMethod(1, "Card"),
+                    new PaymentMethod(2, "Cash")
+                };
+        }
+        public string Name => "pay";
+        public string Description => "Payment method";
+        public bool CanExecute => !authenticationSerivce.IsUserAuthenticated;
+        public void Execute(float price)
+        {
+            int paymentMethodIndex = buyView.AskForPaymentMethod(paymentMethods);
+
+
+        }
+    }
+}
