fdf9ce782ae05797aebb410d50c5c92bc1fc8106
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-01-20 12:22:11 +0200

    building temporary handling exception for payment  in buy use case

--- Homework_VM/VM/UseCases/BuyUseCase.cs
+++ Homework_VM/VM/UseCases/BuyUseCase.cs
@@ -11,6 +11,7 @@ namespace iQuest.VendingMachine.UseCases
         private readonly IProductRepository productRepository;
         private readonly IBuyView buyView;
         private readonly IAuthenticationService authenticationService;
+        private readonly PaymentUseCase paymentUseCase;
 
         public string Name => "Buy";
 
@@ -18,34 +19,44 @@ namespace iQuest.VendingMachine.UseCases
 
         public bool CanExecute => productRepository.GetAll().Any() && !authenticationService.IsUserAuthenticated; 
 
-        public BuyUseCase(IBuyView buyView, IAuthenticationService authenticationService, IProductRepository productRepository)
+        public BuyUseCase(
+            IBuyView buyView, IAuthenticationService authenticationService,
+            IProductRepository productRepository, PaymentUseCase paymentUseCase
+            )
         {
             this.buyView = buyView ?? throw new ArgumentNullException(nameof(buyView));
             this.authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
             this.productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            this.paymentUseCase = paymentUseCase ?? throw new ArgumentNullException(nameof(paymentUseCase));
         }
 
         public void Execute()
-        { 
-            int columnNumber = buyView.RequestProduct();    
-
-            Product? wantedProduct = productRepository.GetProductByColumnId(columnNumber);
-            if (wantedProduct == null)
-            {
-                throw new InvalidColumnNumberException();
-            }
-            if (wantedProduct.Quantity == 0)
-            {
-                throw new ProductNotAvailableException();
-            }
-
+        {
             try
             {
-                PaymentUseCase payment = new PaymentUseCase(buyView, authenticationService);
-                payment.Execute(wantedProduct.Price);
+                int columnNumber = buyView.RequestProduct();    
 
+                Product? wantedProduct = productRepository.GetProductByColumnId(columnNumber);
+                if (wantedProduct == null)
+                {
+                    throw new InvalidColumnNumberException();
+                }
+                if (wantedProduct.Quantity == 0)
+                {
+                    throw new ProductNotAvailableException();
+                }
+
+                paymentUseCase.Execute(wantedProduct.Price);
                 wantedProduct.Quantity--;
                 buyView.DispenseProduct(wantedProduct.Name);
+            }
+            catch (InvalidColumnNumberException e)
+            {
+                Console.WriteLine("Invalid column number!");
+            }
+            catch (ProductNotAvailableException e)
+            {
+                Console.WriteLine("Product not available!");
             }
             catch (InvalidCardNumberException e)
             {
@@ -53,7 +64,7 @@ namespace iQuest.VendingMachine.UseCases
             }
             catch (Exception e)
             {
-                Console.WriteLine("payment service initilization erro");
+                Console.WriteLine("payment service initilization error");
             }   
         }
     }
