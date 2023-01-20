f8ecd73baf64e5d6580409f1d0b1cfb3a84bd07b
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-01-20 18:42:43 +0200

    Card Validator class implementation

--- Homework_VM/VM/UseCases/PaymentUseCase/CardPayment.cs
+++ Homework_VM/VM/UseCases/PaymentUseCase/CardPayment.cs
@@ -6,66 +6,26 @@ namespace iQuest.VendingMachine.UseCases
     internal class CardPayment : IPaymentAlgorithm
     {
         private ICardPaymentTerminal terminal;
-
+        private CardValidator validator;
         public string Name => "Card";
 
         public CardPayment(ICardPaymentTerminal terminal) 
         {
             this.terminal = terminal;
+            this.validator = new CardValidator();
+
         }
 
         public void Run(float price)
         {
             string cardNumber = terminal.AskForCardNumber();
 
-            if(ValidateCardNumber(cardNumber))
+            if(validator.ValidateCardNumber(cardNumber))
                 terminal.TransactionDone();
             else
                 terminal.TransactionFailed();
         }
 
-        public bool ValidateCardNumber(string userInput)
-        {
-            int cardNumberLength = userInput.Length;
-            int sum = 0;
-            bool parity = cardNumberLength % 2 == 0;
-            
-            int[] cardNumber = new int[cardNumberLength];
-            for(int i = 0; i < cardNumberLength; i++)
-                cardNumber[cardNumberLength - 1 - i] = int.Parse(Char.ToString(userInput[i]));
-
-            for (int i = 0; i < cardNumberLength; i++)
-            {
-                bool isOddPositionInNumber = i % 2 != 0;
-
-                if (!isOddPositionInNumber)
-                    sum = sum + cardNumber[i];
-                else
-                {
-                    int newNumberOfOddPosition = cardNumber[i] * 2;
-
-                    if (newNumberOfOddPosition > 9)
-                    {
-                        sum += SumOfDigits(newNumberOfOddPosition);
-                    }
-                    else
-                        sum = sum + newNumberOfOddPosition;
-                }
-            }  
-            return sum % 10 == 0;
-        }
-
-        private int SumOfDigits(int number)
-        {
-            int sum = 0;
-            while (number > 0)
-            {
-                int lastDigit = number % 10;
-                number /= 10;
-
-                sum+= lastDigit;
-            }
-            return sum;
-        }
+ 
     }
 }
