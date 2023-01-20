e70b06397c68214f6ad0761a0f79b9109e65b2d1
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-01-20 12:21:20 +0200

    Create InvalidInputWhilePayingException.cs

---
+++ Homework_VM/VM/Exception/InvalidInputWhilePayingException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iQuest.VendingMachine.Exceptions
+{
+    internal class InvalidInputWhilePayingException : Exception
+    {
+        private const string messageProductNotAvailable = "Invalid input while paying !";
+
+        public InvalidInputWhilePayingException(string message = messageProductNotAvailable)
+            : base(message)
+        {
+        }
+        public InvalidInputWhilePayingException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
