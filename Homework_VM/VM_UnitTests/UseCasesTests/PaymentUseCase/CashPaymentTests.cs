27ffc82aa4d05a2dba3313448de5e28c897f5468
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-01-27 18:24:52 +0200

    Adding unit tests for PaymentUseCase - CashPayment

---
+++ Homework_VM/VM_UnitTests/UseCasesTests/PaymentUseCase/CashPaymentTests.cs
@@ -0,0 +1,48 @@
+﻿using iQuest.VendingMachine.Exceptions;
+using iQuest.VendingMachine.PresentationLayer;
+using iQuest.VendingMachine.UseCases;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VM_UnitTests.UseCasesTests
+{
+    public class CashPaymentTests
+    {
+        private Mock<ICashPaymentTerminal> terminal;
+        private CashPayment cashPayment;
+
+        private float priceToPay = 5.99f;
+
+        public CashPaymentTests()
+        {
+            this.terminal = new Mock<ICashPaymentTerminal>();
+        }
+
+        [Fact]
+        public void WhilePaying_WithNonFloatInput_ThrowsException()
+        {
+            terminal
+                .SetupSequence(x => x.AskForMoney(priceToPay))
+                .Throws(new InvalidPaymentInputException());
+            cashPayment = new CashPayment(terminal.Object);
+
+            Assert.Throws<InvalidPaymentInputException>(() => cashPayment.Run(priceToPay));
+        }
+
+        [Fact]
+        public void WhilePaying_CommandIsCanceled_ThrowsException()
+        {
+
+            terminal
+                .SetupSequence(x => x.AskForMoney(priceToPay))
+                .Throws(new CancelException());
+            cashPayment = new CashPayment(terminal.Object);
+
+            Assert.Throws<CancelException>(() => cashPayment.Run(priceToPay));
+        }
+    }
+}
