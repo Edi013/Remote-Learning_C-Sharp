6a40fca6bb48c5d6bb1e358035cca180a43057d3
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-02-18 10:15:53 +0200

    Creating SqlServerRepo class and a static class to set up the connection

---
+++ Homework_VM/VM/DataAcces/SqlServerConnection.cs
@@ -0,0 +1,20 @@
+﻿using iQuest.VendingMachine.DataLayer;
+using Microsoft.Extensions.Configuration;
+
+public static class SqlServerConnection
+{
+    private static IConfiguration _configuration;
+
+    public static SqlServerRepository SetUp()
+    {
+        var builder = new ConfigurationBuilder()
+               .SetBasePath("D:\\technologii\\GitHubDesktop\\NagarroRepo\\RL_Csharp\\Homework_VM\\VM\\") //Directory.GetCurrentDirectory()
+               .AddJsonFile("appsettings.json", false, true);
+
+        _configuration = builder.Build();
+
+        SqlServerRepository database = new SqlServerRepository(_configuration);
+
+       return database;
+    }
+}
---
+++ Homework_VM/VM/DataAcces/SqlServerRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace iQuest.VendingMachine.DataLayer
+{
+    public class SqlServerRepository : IProductRepository
+    {
+        private string _connectionString;
+
+        public SqlServerRepository(IConfiguration configuration)
+        {
+            Console.WriteLine("Connection string :" + configuration.GetConnectionString("Default"));
+            _connectionString = configuration.GetConnectionString("Default");
+        }
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>();
+
+            try
+            {
+                using var connection = new SqlConnection(_connectionString);
+
+                string queryString = "Select * From dbo.Products";
+                var command = new SqlCommand(queryString, connection);
+                //command.CommandText = System.Data.CommandType.StoredProcedure;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = command;
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                     products.Add(new Product
+                     {
+                         ColumnId = Convert.ToInt32(reader["ColumnId"]),
+                         Name = reader["Name"].ToString(),
+                         Price = Convert.ToSingle(reader["Price"]),
+                         Quantity = Convert.ToInt32(reader["Quantity"])
+                     });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in SqlServerRepo - line 41 ~");
+                Console.WriteLine(e.Message);
+
+            }
+
+            return products;
+        }
+
+        public Product? GetProductByColumnId(int columnId)
+        {
+            var products = GetProducts();
+            if (!products.Any())
+                return new Product { Quantity = 0 };
+
+            return products.FirstOrDefault(x => x.ColumnId == columnId);
+        }
+    }
+}
