a5526a1b412d075987f0d1538ecb7cb557fa7464
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-02-19 09:09:05 +0200

    Implement the new method for the in memory repo

--- Homework_VM/VM/DataAcces/ProductRepository.cs
+++ Homework_VM/VM/DataAcces/ProductRepository.cs
@@ -33,7 +33,7 @@ namespace iQuest.VendingMachine.DataLayer
 
         public void DecreaseQuantity(Product product)
         {
-            throw new NotImplementedException();
+            product.Quantity--;
         }
     }
 }
