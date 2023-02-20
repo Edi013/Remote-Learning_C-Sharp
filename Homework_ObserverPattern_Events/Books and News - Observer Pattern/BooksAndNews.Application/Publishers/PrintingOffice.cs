f6526c09446b8604980bbed25ffb2d22558ef1a2
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-02-20 13:10:34 +0200

    Creating  events, handlers and a function that triggers events

--- Homework_ObserverPattern_Events/Books and News - Observer Pattern/BooksAndNews.Application/Publishers/PrintingOffice.cs
+++ Homework_ObserverPattern_Events/Books and News - Observer Pattern/BooksAndNews.Application/Publishers/PrintingOffice.cs
@@ -1,4 +1,8 @@
 ﻿using iQuest.BooksAndNews.Application.DataAccess;
+using iQuest.BooksAndNews.Application.Publications;
+using iQuest.BooksAndNews.Application.Subscribers;
+using System;
+using System.Collections.Generic;
 
 namespace iQuest.BooksAndNews.Application.Publishers
 {
@@ -13,12 +17,69 @@ namespace iQuest.BooksAndNews.Application.Publishers
     /// </summary>
     public class PrintingOffice
     {
+        private IBookRepository _bookRepository;
+        private INewspaperRepository _newspaperRepository;
+        private ILog _logger;
+
+        private List<ISubscriber> subscribers;
+
+        public event EventHandler<CustomEvent> BookRelease;
+        public event EventHandler<CustomEvent> NewspaperRelease;
+
+
         public PrintingOffice(IBookRepository bookRepository, INewspaperRepository newspaperRepository, ILog log)
         {
+            _bookRepository = bookRepository;
+            _newspaperRepository = newspaperRepository;
+            _logger = log;
+
+            subscribers = new List<ISubscriber>();
         }
 
-        public void PrintRandom(int bookCount, int newspaperCount)
+        public void PrintRandom(int bookCount, int newspaperCount) //
         {
+            for(int i = 0; i < bookCount; i++)
+            {
+                Console.WriteLine("-------NEW BOOK------------");
+                string aux = _bookRepository.GetRandom().ToString();
+                Console.WriteLine("Book printed" + aux);
+                HandlerBookRelease(new CustomEvent(aux));
+            }
+            for(int i = 0; i < newspaperCount; i++)
+            {
+                Console.WriteLine("-------NEW NEWSPAPER------------");
+                string aux = _newspaperRepository.GetRandom().ToString();
+                Console.WriteLine("Newspaper printed" + aux);
+                HandlerNewspaperRelease(new CustomEvent(aux));
+            }
+        }
+        public void HandlerBookRelease(CustomEvent e)
+        {
+            EventHandler<CustomEvent> raiseEvent = BookRelease;
+
+            if(raiseEvent != null)
+            {
+                raiseEvent(this, e);
+            }
+        }
+        public void HandlerNewspaperRelease(CustomEvent e)
+        {
+            EventHandler<CustomEvent> raiseEvent = NewspaperRelease;
+
+            if (raiseEvent != null)
+            {
+                raiseEvent(this, e);
+            }
+        }
+
+
+        public void Subscribe(ISubscriber subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
+        public void Unsubscribe(ISubscriber subscriber)
+        {
+            subscribers.Remove(subscriber);
         }
     }
 }
