c1acd04ae05592c3532a31fe1e62ba040df362a8
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-02-24 10:10:27 +0200

    Update IProductRepository.cs

--- Homework_VM/VendingMachine.Business/IProductRepository.cs
+++ Homework_VM/VendingMachine.Business/IProductRepository.cs
@@ -1,13 +1,6 @@
-﻿using iQuest.VendingMachine.DataLayer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace iQuest.VendingMachine.DataLayer
+﻿namespace iQuest.VendingMachine.DataLayer
 {
-    internal interface IProductRepository
+    public interface IProductRepository
     {
         private static List<Product> products;
         public List<Product> GetProducts();
