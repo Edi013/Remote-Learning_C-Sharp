1327b2188877f134b6520dae7437d30194a6fbd2
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-02-24 15:05:50 +0200

    Refactioring the view of LiteDBRepository class

--- Homework_VM/VendingMachine.DataAccessLibrary/LiteDBRepository.cs
+++ Homework_VM/VendingMachine.DataAccessLibrary/LiteDBRepository.cs
@@ -7,23 +7,26 @@ namespace iQuest.VendingMachine.DataAcces
     public class LiteDBRepository : IProductRepository
     {
         private string _connectionString;
+     
         public LiteDBRepository(string connectionString)
         {
             _connectionString = connectionString;
-
-           /* var products = new List<Product>() {
-                new Product(1, "7Days", 4.99F, 10),
-                new Product(2, "Rolls", 3.99F, 7),
-                new Product(3, "Napoleon", 2.99F, 1),
-                new Product(4, "Ice coffee", 9.99F, 12)
-            };
 
             using (var database = new LiteDatabase(
                         _connectionString))
             {
                 var collection = database.GetCollection<Product>("Products");
-                collection.InsertBulk(products);
-            }*/
+                if (collection.Count() == 0)
+                {
+                    var products = new List<Product>() {
+                        new Product(1, "7Days", 4.99F, 10),
+                        new Product(2, "Rolls", 3.99F, 7),
+                        new Product(3, "Napoleon", 2.99F, 1),
+                        new Product(4, "Ice coffee", 9.99F, 12)
+                    };
+                    collection.InsertBulk(products);
+                }
+            }
         }
 
         public void DecreaseQuantity(Product product)
@@ -31,7 +34,7 @@ namespace iQuest.VendingMachine.DataAcces
             product.Quantity--;
 
             using (var database = new LiteDatabase(
-                        Directory.GetCurrentDirectory()))
+                        _connectionString))
             {
                 var collection = database.GetCollection<Product>("Products");
                 collection.Update(product);
@@ -60,9 +63,9 @@ namespace iQuest.VendingMachine.DataAcces
                         _connectionString))
             {
                 var collection = database.GetCollection<Product>("Products");
+                
                 products = collection.FindAll().ToList();
             }
-
             return products;
         }
     }
