78cfd9233dca9a4f216d73aad6d6af7014ee2682
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-02-27 18:16:24 +0200

    Add events for 2 actions : printing a book, printing a newspapper. And delete the generic event ' CustomEvent'

---
+++ Homework_ObserverPattern_Events/Books and News - Observer Pattern/BooksAndNews.Application/PrintNewspapperEvent.cs
@@ -0,0 +1,14 @@
+﻿using iQuest.BooksAndNews.Application.Publications;
+using System;
+
+namespace iQuest.BooksAndNews.Application
+{
+    public class PrintNewspapper : EventArgs
+    {
+        public Newspaper PrintedNewspapper { get; set; }
+        public PrintNewspapper(Newspaper item)
+        {
+            PrintedNewspapper = item;
+        }
+    }
+}
