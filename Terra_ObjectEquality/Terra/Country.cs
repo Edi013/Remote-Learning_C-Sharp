37faadb3088bb4cee2b9fce6d466eee4b7ef0892
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-03-06 09:39:21 +0200

    Exercise 1 - ComparableTests are passed - Equality Tests

--- Terra_ObjectEquality/Terra/Country.cs
+++ Terra_ObjectEquality/Terra/Country.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace iQuest.Terra
 {
-    public class Country
+    public class Country : IEquatable<Country>, IComparable<Country>
     {
         public string Name { get; }
 
@@ -12,6 +13,39 @@ namespace iQuest.Terra
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
             Capital = capital ?? throw new ArgumentNullException(nameof(capital));
+        }
+
+        public bool Equals([AllowNull] Country other)
+        {
+            if (other == null) 
+                return false;
+            if (ReferenceEquals(this, other)) 
+                return true;
+            if (this.GetType() != other.GetType())
+                return false;
+
+
+            return this.Name == other.Name 
+                && this.Capital == other.Capital
+                ? true
+                : false ;
+        }
+        public override bool Equals(object obj)
+        {
+            return this.GetType() == obj.GetType() 
+                ? Equals(obj) 
+                : false;
+        }
+
+        public int CompareTo([AllowNull] Country other)
+        {
+            if(other == null) return 1;
+            if (ReferenceEquals(this, other)) return 0;
+            return 5;
+        }
+        public override int CompareTo()
+        {
+
         }
     }
 }
