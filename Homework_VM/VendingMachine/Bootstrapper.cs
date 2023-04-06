012f3d0c3123b2bba7a4e00c90101485ba95eb08
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-06 15:02:04 +0300

    Removing autofac implementation from Bootstrapper
    This action is part of 'modularizing autofac container' process.

--- Homework_VM/VendingMachine/Bootstrapper.cs
+++ Homework_VM/VendingMachine/Bootstrapper.cs
@@ -1,8 +1,5 @@
 ﻿using Autofac;
-using System.Configuration;
 using iQuest.VendingMachine.Business;
-using iQuest.VendingMachine.DataAcces;
-using iQuest.VendingMachine.Presentation;
 
 namespace iQuest.VendingMachine
 {
@@ -10,86 +7,12 @@ namespace iQuest.VendingMachine
     {
         public void Run()
         {
-            var container = BuildAutofacContainer();
+            var container = AutofacContainer.GetInstance();
             using (var scope = container.BeginLifetimeScope())
             {
                 var app = scope.Resolve<VendingMachineApplication>();
                 app.Run();
             }
-        }
-
-        private static IContainer BuildAutofacContainer()
-        {
-            var builder = new ContainerBuilder();
-
-            builder.RegisterType<CardPayment>()
-                 .As<IPaymentAlgorithm>();
-            builder.RegisterType<CashPayment>()
-                 .As<IPaymentAlgorithm>();
-            builder.RegisterType<PaymentUseCase>()
-                .As<IPaymentUseCase>();
-            builder.RegisterType<CardValidator>()
-                .AsSelf();
-
-            builder.RegisterType<BuyUseCase>()
-                .As<IUseCase>();
-            builder.RegisterType<LoginUseCase>()
-                .As<IUseCase>();
-            builder.RegisterType<LogoutUseCase>()
-                .As<IUseCase>();
-            builder.RegisterType<LookUseCase>()
-                .As<IUseCase>();
-            builder.RegisterType<PaymentUseCase>()
-                .As<IPaymentUseCase>();
-            builder.RegisterType<TurnOffUseCase>()
-                .AsSelf();
-
-            builder.RegisterType<TurnOffService>()
-                .As<ITurnOffService>()
-                .SingleInstance();
-            builder.RegisterType<AuthenticationService>()
-                .As<IAuthenticationService>()
-                .SingleInstance();
-            builder.RegisterType<PaymentMethod>()
-                .AsSelf();
-
-            string _connectionString;
-            switch (ConfigurationManager.AppSettings["repoType"])
-            {
-                case "InMemory":
-                    builder.RegisterType<InMemoryRepository>()
-                        .As<IProductRepository>();
-                    break;
-                case "SQL":
-                    _connectionString =
-                        ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString;
-                    builder.Register<SqlServerRepository>(_ => new SqlServerRepository(_connectionString))
-                        .As<IProductRepository>();
-                    break;
-                case "LiteDB":
-                    _connectionString =
-                        ConfigurationManager.ConnectionStrings["LiteDB"].ConnectionString;
-                    builder.Register<LiteDBRepository>(_ => new LiteDBRepository(_connectionString))
-                        .As<IProductRepository>();
-                    break;
-            }
-
-            builder.RegisterType<ShelfView>()
-                .As<IShelfView>();
-            builder.RegisterType<BuyView>()
-                .As<IBuyView>();
-            builder.RegisterType<MainDisplay>()
-                 .As<IMainDisplay>();
-            builder.RegisterType<CashPaymentTerminal>()
-                .As<ICashPaymentTerminal>();
-            builder.RegisterType<CardPaymentTerminal>()
-                .As<ICardPaymentTerminal>();
-
-            builder.RegisterType<VendingMachineApplication>()
-                .As<VendingMachineApplication>()
-                .SingleInstance();
-
-            return builder.Build();
         }
     }
 }
