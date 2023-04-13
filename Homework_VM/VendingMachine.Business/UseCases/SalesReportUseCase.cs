453c1acda91ce03ced716bb1aeeb3b2fc0d168c9
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-13 08:27:38 +0300

    Create usecases for each report

---
+++ Homework_VM/VendingMachine.Business/UseCases/SalesReportUseCase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iQuest.VendingMachine.Business
+{
+    public class SalesReportUseCase : IUseCase
+    {
+        public void Execute()
+        {
+        }
+    }
+}
---
+++ Homework_VM/VendingMachine.Business/UseCases/StockReportUseCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iQuest.VendingMachine.Business
+{
+    public class StockReportUseCase : IUseCase
+    {
+        IProductRepository productRepository;
+
+        public StockReportUseCase(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public void Execute()
+        {
+
+        }
+    }
+}
