c3ac54065462dcc9c0ce90196232dd2595823945
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-17 19:19:49 +0300

    Prompter for reports.
    To notice that Stock report doesn't use this prompter. It doesn't need it

---
+++ Homework_VM/VendingMachine.Presentation/PresentationLayer/ReportsView.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VendingMachine.Presentation.PresentationLayer
+{
+    public class ReportsView
+    {
+        private DateTime RequestDateTimeForReport(string displayedRequest)
+        {
+            DateTime result;
+            while (true)
+            {
+                Console.WriteLine("Input report "+ displayedRequest + " date in the format \"YYYY-MM-DD\":");
+                if (DateTime.TryParse(Console.ReadLine(), out result))
+                    return result;
+                Console.WriteLine("Invalid / wrong date format.");
+            }
+        }
+        private DateTime RequestReportStartDate()
+        {
+            return RequestDateTimeForReport("start");
+        }
+        private DateTime RequestReportEndDate()
+        {
+            return RequestDateTimeForReport("end");
+        }
+
+        public TimeInterval AskForTimeInterval()
+        {
+            return new TimeInterval
+                (RequestReportStartDate(), RequestReportEndDate());
+        }
+        public void DisplaySuccessMessage([Optional] string displayInformationOnNewLine)
+        {
+            Console.WriteLine("Operation succeded.\n" + displayInformationOnNewLine);
+        }
+        
+    }
+}
---
+++ Homework_VM/VendingMachine.Presentation/PresentationLayer/TimeInterval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Presentation.PresentationLayer
+{
+    public class TimeInterval
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public TimeInterval(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
