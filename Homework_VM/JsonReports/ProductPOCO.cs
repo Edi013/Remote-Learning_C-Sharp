fbf8416ef70f4820073b541b5defd7dbf68fd1ac
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-17 19:11:20 +0300

    Adding a poco for products

---
+++ Homework_VM/JsonReports/ProductPOCO.cs
@@ -0,0 +1,13 @@
+﻿namespace iQuest.VendingMachine.JsonReports
+{
+    public class ProductPOCO
+    {
+        public string Name;
+        public int Quantity;
+        public ProductPOCO(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
