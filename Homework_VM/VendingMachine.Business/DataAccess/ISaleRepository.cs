5812ba16f78d987e1617c2524cb5b97fb72cf12e
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-20 14:18:51 +0300

    Creating Sales repository and its interface

---
+++ Homework_VM/VendingMachine.Business/DataAccess/ISaleRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace iQuest.VendingMachine.Business
+{
+    public interface ISaleRepository
+    {
+        public IEnumerable<Sale> Get(TimeInterval interval);
+        public IEnumerable<ProductSale> GetGroupedByProduct(TimeInterval interval);
+        public void Add(Sale sale);
+    }
+}
