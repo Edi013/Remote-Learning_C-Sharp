dac650ad3c0d869dae7a0f7f5b8f249d0c1322d2
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-20 15:38:27 +0300

    Implementing volume report use case

--- Homework_VM/VendingMachine.Business/UseCases/VolumeReportUseCase.cs
+++ Homework_VM/VendingMachine.Business/UseCases/VolumeReportUseCase.cs
@@ -8,8 +8,23 @@ namespace iQuest.VendingMachine.Business
 {
     public class VolumeReportUseCase : IUseCase
     {
+        ISaleRepository saleRepository;
+        IReportRepository<VolumeReport> reportRepository;
+        IReportsView reportsView;
+        public VolumeReportUseCase(ISaleRepository saleRepository,
+            IReportRepository<VolumeReport> reportRepository, IReportsView reportsView)
+        {
+            this.saleRepository = saleRepository;
+            this.reportRepository = reportRepository;
+            this.reportsView = reportsView;
+        }
+
         public void Execute()
         {
+            reportRepository.Add(
+                new VolumeReport(
+                    saleRepository.GetGroupedByProduct(
+                         reportsView.AskForTimeInterval())));
         }
     }
 }
