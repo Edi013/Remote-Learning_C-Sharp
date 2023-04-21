4a947980365e2f95711502ff1a49709014c68c8c
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-21 13:01:24 +0300

    Creating SupplyExistingProductUseCase, Command and the smallest class used - QuantitySupply

---
+++ Homework_VM/VendingMachine.Business/UseCases/SupplyExistingProductUseCase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iQuest.VendingMachine.Business
+{
+    public class SupplyExistingProductUseCase : IUseCase
+    {
+        IProductRepository productRepository;
+        ISupplyProductView supplyProductView;
+
+        public SupplyExistingProductUseCase(IProductRepository productRepository, ISupplyProductView supplyProductView)
+        {
+            this.productRepository = productRepository;
+            this.supplyProductView = supplyProductView;
+        }
+
+        public void Execute()
+        {
+            productRepository.IncreaseQuantity(
+                supplyProductView.RequestProductQuantity());
+        }
+    }
+}
---
+++ Homework_VM/VendingMachine.Presentation/PresentationLayer/Commands/SupplyExistingProductCommand.cs
@@ -0,0 +1,25 @@
+﻿using iQuest.VendingMachine.Business;
+
+namespace iQuest.VendingMachine.Presentation
+{
+    public class SupplyExistingProductCommand : ICommand
+    {
+        private readonly IAuthenticationService authenticationService;
+        private IUseCaseFactory factory;
+
+        public string Name => "Update product";
+        public string Description => "Updates a product";
+        public bool CanExecute => authenticationService.IsUserAuthenticated;
+
+        public SupplyExistingProductCommand(IUseCaseFactory factory, IAuthenticationService authenticationService)
+        {
+            this.factory = factory;
+            this.authenticationService = authenticationService;
+        }
+        public void Execute()
+        {
+            var useCase = factory.Create<SupplyExistingProductUseCase>();
+            useCase.Execute();
+        }
+    }
+}
