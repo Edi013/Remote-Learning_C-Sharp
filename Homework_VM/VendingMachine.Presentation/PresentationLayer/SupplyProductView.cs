2fd7f323d8c90b3853f129d83a4276ca966e496c
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-21 13:01:45 +0300

    Creating the prompter for the Supply use cases

---
+++ Homework_VM/VendingMachine.Presentation/PresentationLayer/SupplyProductView.cs
@@ -0,0 +1,75 @@
+﻿using iQuest.VendingMachine.Business;
+
+namespace iQuest.VendingMachine.Presentation
+{
+    public class SupplyProductView : ISupplyProductView
+    {
+        public Product RequestNewProduct()
+        {
+            Console.WriteLine("Input data for new product:");
+            var result = new Product();
+            var properties = typeof(Product).GetProperties();
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"Input {property.Name} as {property.DeclaringType}");
+
+                dynamic userInput;
+                while (true)
+                {
+                    userInput = Console.ReadLine();
+                    if (property.PropertyType == typeof(string))
+                        break;
+                    if (property.PropertyType == typeof(float))
+                    {
+                        float auxInput;
+                        float.TryParse(userInput, out auxInput);
+                        userInput = auxInput;
+                        break;
+                    }
+                    if (property.PropertyType == typeof(int))
+                    {
+                        int auxInput;
+                        int.TryParse(userInput, out auxInput);
+                        userInput = auxInput;
+                        break;
+                    }
+                    Console.WriteLine("Invalid data type. Try again.");
+                }
+
+                property.SetValue(result, userInput);
+            }
+            return result;
+        }
+
+
+        public QuantitySupply RequestProductQuantity()
+        {
+            Console.WriteLine("Input data to supply a product:");
+
+            var quantitySupply = new QuantitySupply();
+            var properties = typeof(QuantitySupply).GetProperties();
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"Input {property.Name} as {property.DeclaringType}");
+
+                dynamic userInput;
+                while (true)
+                {
+                    userInput = Console.ReadLine();
+                    if (property.PropertyType == typeof(int))
+                    {
+                        int auxInput;
+                        int.TryParse(userInput, out auxInput);
+                        userInput = auxInput;
+                        break;
+                    }
+                    Console.WriteLine("Invalid data type. Try again.");
+                }
+
+                property.SetValue(quantitySupply, userInput);
+            }
+            return quantitySupply;
+        }
+    }
+}
