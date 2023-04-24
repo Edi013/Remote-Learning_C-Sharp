f71ac39e781c1de3b432c5544bc968a75cd7bef8
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-24 16:24:56 +0300

    Implementing xml class library

---
+++ Homework_VM/XmlReports/SalesReport/XmlSalesReportRepository.cs
@@ -0,0 +1,36 @@
+﻿using iQuest.VendingMachine.Business;
+
+namespace iQuest.VendingMachine.XmlReports
+{
+    public class XmlSalesReportRepository : XmlFileReportsRepository<SalesReportContent>, IReportRepository<SalesReport>
+    {
+        private SalesReportContent CreateReportContent(SalesReport salesReport)
+        {
+            var content = new SalesReportContent();
+
+            foreach (var item in salesReport)
+            {
+                content.Add(
+                    new SalesReportProduct(
+                       item.Date, item.ProductName, item.Price, item.PaymentMethod));
+            }
+
+            return content;
+        }
+        private XmlReport<SalesReportContent> ToJsonStockReport(SalesReport salesReport)
+        {
+            var content = CreateReportContent(salesReport);
+            return new XmlReport<SalesReportContent>()
+            {
+                ReportName = salesReport.Name,
+                GeneratedTime = salesReport.GeneratedTime,
+                Content = content
+            };
+        }
+
+        public void Add(SalesReport salesReport)
+        {
+            GenerateFile(ToJsonStockReport(salesReport));
+        }
+    }
+}
---
+++ Homework_VM/XmlReports/XmlReport.cs
@@ -0,0 +1,10 @@
+﻿namespace iQuest.VendingMachine.XmlReports
+{
+    public class XmlReport<XmlContent>
+    {
+        public DateTime GeneratedTime { get; set; }
+
+        public string ReportName { get; set; }
+        public XmlContent Content { get; set; }
+    }
+}
